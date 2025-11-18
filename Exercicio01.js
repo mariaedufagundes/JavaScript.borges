ee5c9a12ebf06c53e9845d9059aa73a5d85b109f
MARIA EDUARDA FAGUNDES <54467915842@imaccommand035.local>
2025-11-18 10:46:52 -0300

    Exercicio01

---
+++ Exercicio01.js
@@ -0,0 +1,10 @@
+let usuario = prompt("Digite o seu usuario: ")
+
+let senha = prompt("Digite a sua senha: ")
+
+if(usuario == "admin" && senha == "senha123"){
+console.log("usuario bem sucedido!")
+}
+else{
+    console.log("você nao tem permissao de acesso!")
+}
---
+++ exemplo.js
@@ -0,0 +1,58 @@
+//Alert - Apresenta uma mensagem na tela 
+// window.alert("Ola tuma do borges")
+// //Confirm - Pergunta com ok/ cancela (Retornar um True ou false)
+// window.confirm("O Jonas vai casar!")
+// //promp - pega o texto da tela
+// window.prompt("Qual o seu nome?")
+
+//Comentario em linha
+/* 
+Comentario em texto 
+*/
+
+// console.log("multimidia")
+
+// let nome = prompt("Informe o nome: ")
+
+// console.log("Bem vindo!" +nome)
+
+// let numero1 =  Number.parseInt (prompt("Digite o primeiro numero: "))
+// let numero2 = Number.parseInt(prompt("Digite o segundo numero: "))
+
+// let resultado = numero1 + numero2
+
+// console.log(resultado)
+
+
+// let numero1 =  Number.parseFloat (prompt("Digite o primeiro numero: "))
+// let numero2 = Number.parseFloat(prompt("Digite o segundo numero: "))
+
+// let resultado = numero1 + numero2
+
+// console.log(resultado)
+
+
+let idade = window.prompt("Qual sua idade?")
+
+if(idade >=18){
+    console.log("Maior de idade! Dalheee")
+}else{
+    console.log("Voce e menor de idade!")
+}
+
+
+switch (idade) {
+    case value:"18"
+    console.log("Maior de idade! Da-lhee")
+        break;
+case"27":
+
+console.log("Esta perto dos 30")
+break;
+
+
+    default:
+console.log("Nada a dizer")
+        break;
+}
+
