a303af665dc765de73cf742bafafb48eae417c26
MARIA EDUARDA FAGUNDES <54467915842@imaccommand035.local>
2025-11-25 13:31:56 -0300

    Exercicio06loop

---
+++ loop/Exercicio06.js
@@ -0,0 +1,12 @@
+let numero = 1
+let soma =0
+
+while(numero <= 100) {
+    soma = soma + numero
+
+    console.log(soma)
+
+    numero ++
+} 
+
+console.assertlog("A soma dos numeros de 1 a 100 é: " + soma)
