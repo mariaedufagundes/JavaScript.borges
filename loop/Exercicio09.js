16369cf4ccea924ce4cd1c22881bd27ad69a2841
MARIA EDUARDA FAGUNDES <54467915842@imaccommand035.local>
2025-11-25 14:20:18 -0300

    Exercicio09loop

---
+++ loop/Exercicio09.js
@@ -0,0 +1,7 @@
+let numero = parseInt(prompt("Digite um número: "));
+
+while (numero !== 0){
+    numero = parseInt(prompt("Número diferente de 0. Digite novamente"));
+}
+
+alert("Você digitou 0. Programa encerrado. "); 
