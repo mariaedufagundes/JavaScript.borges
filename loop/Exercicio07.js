eeb25dd4d290a794ab182a70af4931b789a09854
MARIA EDUARDA FAGUNDES <54467915842@imaccommand035.local>
2025-11-25 13:51:47 -0300

    Exercicio07loop

---
+++ loop/Exercicio07.js
@@ -0,0 +1,10 @@
+let numero = parseInt(prompt("Digite um numero para a contagem regressiva"))
+
+console.log("contagem regressiva esta começando em " + numero)
+
+while(numero <=0){
+    console.log (numero)
+    numero = numero -1 
+}
+
+console.log("contagem regressiva finalizada!")
